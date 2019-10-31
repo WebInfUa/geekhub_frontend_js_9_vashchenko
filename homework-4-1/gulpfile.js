c04523f72840c4261264d5c2f98abe4358bb76e9
WebInfUa <web.inf.ua@gmail.com>
2019-10-31 23:40:11 +0200

    Hw 4 added css minify

--- homework-4-1/gulpfile.js
+++ homework-4-1/gulpfile.js
@@ -1,12 +1,19 @@
 const gulp = require('gulp');
 const gulpSass = require('gulp-sass');
 const gulpSource = require('gulp-sourcemaps');
-const watch = require('gulp-watch');
 
 gulp.task('sass-watch', function () {
   return gulp.src('./src/sass/**/*.scss')
     .pipe(gulpSource.init()) //Показывает строку scss в консоли
-    .pipe(gulpSass().on('Error', gulpSass.logError))
+    .pipe(gulpSass({outputStyle: 'compressed'}).on('Error', gulpSass.logError))
+    /**
+     * nested - вложенный, по умолчанию;
+     * expanded - развернутый;
+     * compact - компактный, когда селектор и его свойства в фигурных скобках выводятся в одну строку;
+     * compressed - сжатый. Кроме того, благодаря обработке .on('error', sass.logError), если возникнет ошибка,
+     * нам не придется перезагружать команду выполенния Gulpfile и мы будем видеть, в какой строке Sass файла у нас ошибка.
+     * В примерах я буду использовать стиль вывода expanded для наглядности.
+     **/
     .pipe(gulpSource.write('./')) //Показывает строку scss в консоле
     .pipe(gulp.dest('./css/'))
 });
