8636338a2171050529632a9e7e5040b260a684fa
WebInfUa <web.inf.ua@gmail.com>
2019-11-15 22:49:43 +0200

    Learn js

--- homework-6-1/firstJS.js
+++ homework-6-1/firstJS.js
@@ -1,10 +1,20 @@
 alert('Hello! Need create the array and output result some manipulation whits this array.');
 let userName = prompt('Enter your name:',);
-console.log('console log output = '+userName);
-console.log(userName.length);
+let status = 0;
+let numberArr = [];
+// console.log('console log output = '+userName);
+// console.log(userName.length);
 
 if (userName !== "" && userName.length >= 3) {
-  document.getElementById('jsHello').innerHTML = '<p class="${userName}"> Hello '+userName.charAt(0).toUpperCase() + userName.slice(1)+'!</p>';
+  document.getElementById('jsHello').innerHTML = '<p class="user__name"> Hello '+userName.charAt(0).toUpperCase() + userName.slice(1)+'!</p>';
+  status = 1;
 } else {
-  document.getElementById('jsHello').innerHTML = '<p class="${userName}"> Hello Stranger!</p>';
+  document.getElementById('jsHello').innerHTML = '<p class="user__name"> Hello Stranger!</p>';
+  status = 1;
+}
+
+(status === 1) ? document.getElementById('jsForm').style.display = 'flex' : window.location.reload(false);
+
+function sendNumber(number) {
+  numberArr.push(number)
 }
