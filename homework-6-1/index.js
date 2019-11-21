faed7f661ef1c6379978f936e0eddf89406363ae
WebInfUa <web.inf.ua@gmail.com>
2019-11-21 23:47:00 +0200

    Some fix after meetings

--- homework-6-1/index.js
+++ homework-6-1/index.js
@@ -1,26 +1,26 @@
-'use strict'
+'use strict';
 
 let numArr = [-35, 26, 48, 78, 19, -6, 0, 33, 71, 12, 23, 29, 100];
 
 //Task1
 let arrOperation = (array) => {
   let arrSum = array.reduce((accumulator, currentValue)  => accumulator + currentValue, 0);
-  let maxValue = Math.max.apply(null, array);
-  let minValue = Math.min.apply(null, array);
+  const maxValue = Math.max(...array);
+  const minValue = Math.min(...array);
   let maxIndex = array.indexOf(maxValue);
   let minIndex = array.indexOf(minValue);
   array[maxIndex] = minValue;
   array[minIndex] = maxValue;
-  let result = 'Sum = '+ arrSum +'\nMax = '+ maxValue +'\nMin = '+ minValue +'\nReplace = '+ numArr;
-  
+  let result = `Sum = ${arrSum} Max  ${maxValue} Min = ${minValue} Replace ${numArr}`;
+
   return (result);
-}
+};
 
 //Task2
 let createSentences = (text, callback) => {
   let sentence = 'Callback = callback function '+ text;
   callback(sentence);
-}
+};
 
 let logWrite = (message) => console.log(message);
 
@@ -35,45 +35,44 @@ let fooBar = (array) => {
       array[i] = 'foo';
     }
   }
-  
+
   return array;
-}
+};
 
 //Task4
 let stringMatches = (frsStr, secStr, callback) => {
   callback('First string = '+ frsStr);
   callback('Second string = '+ secStr);
-  
+
   frsStr = frsStr.replace(/\s/g, '');
   secStr = secStr.replace(/\s/g, '');
   let frsStrArr = [], secStrArr = [];
-  
+
   for (let char of frsStr) {
     frsStrArr.push(char);
   }
-  
+
   for (let char of secStr) {
     secStrArr.push(char);
   }
-  
+
   let missingChar = frsStrArr.filter(n => secStrArr.indexOf(n) === -1);
   let percentOccurrence = Math.round(((frsStrArr.length - missingChar.length) / frsStrArr.length) * 100) + '%';
   let result = 'String coincidence = '+ percentOccurrence;
-  
+
   callback(result);
-}
+};
 
 let randomStr = (strLength) => {
-  let result = ''; 
+  let result = '';
   let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let charactersLength = characters.length;
   for (let i = 0; i < strLength; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
-  
-  return result;
-}
 
+  return result;
+};
 
 logWrite('Array = '+numArr);
 logWrite(arrOperation(numArr));
