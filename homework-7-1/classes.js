5bfa35560999fa2cd8ebd53e4785bce808ca1117
Alex <54575097+WebInfUa@users.noreply.github.com>
2019-11-26 17:26:14 +0200

    Update classes.js

--- homework-7-1/classes.js
+++ homework-7-1/classes.js
@@ -1,10 +1,10 @@
 class Console {
     constructor() {
-			this.histories = [];
+	this.histories = [];
     }
 
     log(...args) {
-			let result = args.map(value => {
+	let result = args.map(value => {
         if (typeof value == 'object') {
          value = JSON.stringify(value);
         }
@@ -13,20 +13,20 @@ class Console {
         }
           return value;
         }).join('');
-			this.histories.push(result);
-			return result;
+	this.histories.push(result);
+	return result;
     }
 
     history(range = [0, this.histories.length]) {
-			let [start, end] = range;
-			if(!range) return this.histories.join('\n');
-      if (end < start) end = start;
-			if (start < 0) start = 1;
-			return this.histories.slice(start - 1, end).join('\n');
+	let [start, end] = range;
+	if(!range) return this.histories.join('\n');
+	if (end < start) end = start;
+	if (start < 0) start = 1;
+	return this.histories.slice(start - 1, end).join('\n');
     }
 
     clearHistory() {
-			this.histories = [];
-			return true;
+	this.histories = [];
+	return true;
     }
 }
