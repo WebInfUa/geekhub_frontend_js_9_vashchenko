eb153b0c880bff91bde25922c7a287942d442dab
WebInfUa <web.inf.ua@gmail.com>
2019-12-17 10:42:37 +0200

    bootstrap and user interface

---
+++ hpomework-10-1/js/main.js
@@ -0,0 +1,33 @@
+let nodeFetch = require('node-fetch');
+
+async function getData(url) {
+  try {
+    let data = await nodeFetch(url);
+    let res;
+    res = await data.json();
+    return res;
+  } catch (e) {
+    throw "Ou ou, we have some problem. Check you URL or try use default URL";
+  }
+}
+
+async function takeData() {
+  const url = document.getElementById('serverUrl').value;
+  try {
+    document.getElementById('serverResult').value = await getData(url);
+    console.log('function getData: Server request is - Successful');
+  }catch (e) {
+    document.getElementById('serverResult').value = e;
+    console.log('Function takeData - take promise error');
+  }
+}
+
+function defUrl() {
+  let status = document.getElementById('optionsRadios1').checked;
+  if (status) {
+    document.getElementById('serverUrl').value='https://my-json-server.typicode.com/typicode/demo/posts'
+  } else {
+    document.getElementById('serverUrl').value=''
+  }
+}
+
