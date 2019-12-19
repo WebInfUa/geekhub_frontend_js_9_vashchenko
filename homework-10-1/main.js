92fc206fb7c0266bdd49ef804d955486d5120f15
WebInfUa <web.inf.ua@gmail.com>
2019-12-19 10:43:27 +0200

    Fetch some error

--- homework-10-1/main.js
+++ homework-10-1/main.js
@@ -1,36 +1,46 @@
 'use strict';
 
-const nodeFetch = require("node-fetch");
-
 async function getData(url) {
+  debugger;
   console.log(url);
-  try {
-    let data = await nodeFetch(url);
+    debugger;
+    let data = await fetch(url);
     let res;
-    res = await data.json();
-    return res;
-  } catch (e) {
-    throw "Ou ou, we have some problem. Check you URL or try use default URL";
+  if (data.ok) {
+      debugger;
+      res = await data.text();
+      debugger;
+      return res;
+  } else {
+      res = "Ou ou, we have some problem. Check you URL or try use default URL";
+      debugger;
+      return res;
   }
 }
 
 async function takeData() {
+  debugger;
   const url = document.getElementById('serverUrl').value;
+  debugger;
   console.log(`take data url - ${url}`);
-  try {
-    document.getElementById('serverResult').value = await getData(url);
+  debugger;
+  let result;
+    debugger;
+    result = await getData(url);
+    debugger;
+    document.getElementById('serverResult').value = result;
+    debugger;
     console.log('function getData: Server request is - Successful');
-  }catch (e) {
-    document.getElementById('serverResult').value = e;
-    console.log('Function takeData - take promise error');
-  }
 }
 
 function defUrl() {
+  debugger;
   let status = document.getElementById('optionsRadios1').checked;
   if (status) {
-    document.getElementById('serverUrl').value='https://my-json-server.typicode.com/typicode/demo/posts'
+    debugger;
+    document.getElementById('serverUrl').value='https://my-json-server.typicode.com/typicode/demo/posts';
   } else {
+    debugger;
     document.getElementById('serverUrl').value=''
   }
 }
