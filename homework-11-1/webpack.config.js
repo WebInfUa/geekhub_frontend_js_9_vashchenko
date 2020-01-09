ee88482baa94cf73acbb7709bc09786e51881410
WebInfUa <web.inf.ua@gmail.com>
2020-01-09 18:17:29 +0200

    HW_11

--- homework-11-1/webpack.config.js
+++ homework-11-1/webpack.config.js
@@ -1,13 +1,14 @@
 'use strict';
 const path = require('path');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const htmlWebpackPlug = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/scripts/main.js',
+  entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, './dist/js'),
-    filename: 'build.js',
-    publicPath: 'dist/js/'
+    path: path.resolve(__dirname, './dist'),
+    filename: 'js/build.js',
+    publicPath: ''
   },
   devServer: {
     overlay: true
@@ -19,15 +20,36 @@ module.exports = {
         loader: 'babel-loader',
       },
       {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
           fallback: "style-loader",
           use: "css-loader"
         })
+      },
+      {
+        test: /\.(jpe?g|png|svg)$/i,
+        loader: 'file-loader',
+        options: {
+          publicPath: 'assets/images',
+          name: '[name].[ext]',
+          outputPath: 'images',
+        }
       }
     ]
   },
   plugins: [
-    new ExtractTextPlugin("../css/styles.css")
+    new ExtractTextPlugin('css/styles.css'),
+    new htmlWebpackPlug({
+      template: path.join(__dirname, 'src', 'index.html'),
+      hash: true
+    })
   ]
 };
