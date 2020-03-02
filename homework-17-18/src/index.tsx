38272f88473dedf062e93fcc5cd8de2a1d94b0bf
WebInfUa <web.inf.ua@gmail.com>
2020-03-03 00:14:49 +0200

    add redux

--- homework-17-18/src/index.tsx
+++ homework-17-18/src/index.tsx
@@ -8,6 +8,16 @@ import AllReducers from "./reducers";
 
 const store = createStore (AllReducers);
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App'
+import * as serviceWorker from './serviceWorker';
+import {Provider} from "react-redux";
+import store from "./redux/store/storeConfig";
+
+
+
 ReactDOM.render(
 <Provider store={store}>
   <User />
